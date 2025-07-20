0352c56aeb62d757b8573c1d569d8170f1390ff8
Sudipto Roy <sroy@miu.edu>
2025-07-19 23:20:58 -0500

    Observable and environment

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -5,15 +5,42 @@
 //  Created by Sudipto Roy on 7/19/25.
 //
 
+import Observation
 import SwiftUI
 
-struct ContentView: View {
+@Observable
+class User {
+    var firstName = "Sudipto"
+    var lastName = "Roy"
+}
+
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    var name: String = "Sudipto"
     var body: some View {
+        Text(name)
+        Button("Dismiss") {
+            dismiss()
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var user = User()
+    @State private var showingSheet = false
+    
+    var body: some View {
+        Button("Show Sheet") {
+            showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: user.firstName)
+        }
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            TextField("First Name", text: $user.firstName)
+            TextField("last Name", text: $user.lastName)
         }
         .padding()
     }
