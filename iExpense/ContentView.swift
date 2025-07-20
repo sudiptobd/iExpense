1c40b5fd0dddc59459dc6d4600b1c19ef4952a68
Sudipto Roy <sroy@miu.edu>
2025-07-20 01:09:13 -0500

    json decoder

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,8 +8,7 @@
 import Observation
 import SwiftUI
 
-@Observable
-class User {
+struct User: Codable {
     var firstName = "Sudipto"
     var lastName = "Roy"
 }
@@ -26,13 +25,29 @@ struct SecondView: View {
 }
 
 struct ContentView: View {
-    @AppStorage("tap") private var tapCount = 0
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
     
     var body: some View {
-        Button("Tap Count \(tapCount)") {
-            tapCount += 1
-            UserDefaults.standard.set(tapCount, forKey: "tap")
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            if let data = try? encoder.encode(user) {
+                UserDefaults.standard.set(data, forKey: "user")
+            }
         }
+        
+        Text("User is \(user.firstName) \(user.lastName)")
+        
+        Button("Load User") {
+            if let data = UserDefaults.standard.data(forKey: "user") {
+                let decoder = JSONDecoder()
+                if var loadedUser = try? decoder.decode(User.self, from: data) {
+                    loadedUser.firstName = "Sudipto"
+                    user = loadedUser
+                }
+            }
+        }
+        
+        
     }
 }
 
